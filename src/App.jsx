a250891c96516d37bf867651b06ac6e4b644664f
Riska Agung <riskaagung11@gmail.com>
2023-06-13 06:29:24 +0700

    Building a First Custom Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <h1>Let&apos;s get started</h1>;
+  return (
+    <>
+      <h1>Let&apos;s get started</h1>
+      <ExpenseItem></ExpenseItem>
+    </>
+  );
 }
 
 export default App;
