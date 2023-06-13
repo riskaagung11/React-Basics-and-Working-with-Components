6a43a790a971beb23734478d26d2710dd8a79316
Riska Agung <riskaagung11@gmail.com>
2023-06-13 09:36:40 +0700

    Passing Data via 'props'

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,12 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
-  const expanseDate = new Date(2021, 2, 28);
-  const expanseTitle = "Car Insurance";
-  const expanseAmount = 294.67;
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{expanseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expanseTitle}</h2>
-        <div className="expense-item__price">${expanseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
